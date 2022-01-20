b25e58670de2e91cb418abf6b5997615a764c50b
Renz <renzcarillo32@gmail.com>
2022-01-20 19:10:17 +0800

    chore: added favicon

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,6 +9,12 @@ import Main from '../components/Homepage/Main';
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Covid-19</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       <Nav></Nav>
       <Main></Main>
       <Infographic></Infographic>
